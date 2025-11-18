8349aa7b4b2add57c47290a960ae46c91ca95051
Xhensila Hoxha <xhensila.hoxha@fau.de>
2025-11-18 16:03:53 +0100

    Implementation: Homework B03. Changed: AbstractName.ts, StringArrayName.ts, StringName.ts

--- src/adap-b03/names/AbstractName.ts
+++ src/adap-b03/names/AbstractName.ts
@@ -1,3 +1,4 @@
+import { escape } from "querystring";
 import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
 
@@ -6,15 +7,34 @@ export abstract class AbstractName implements Name {
     protected delimiter: string = DEFAULT_DELIMITER;
 
     constructor(delimiter: string = DEFAULT_DELIMITER) {
-        throw new Error("needs implementation or deletion");
+        this.delimiter = delimiter;
     }
 
-    public clone(): Name {
-        throw new Error("needs implementation or deletion");
-    }
+    //subclass-dependent
+    public abstract clone(): Name;
 
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        const rawComponents: string[] = [];
+        for(let i=0; i<this.getNoComponents(); i++){
+            const masked = this.getComponent(i);
+            let unmasked = '';
+
+            for(let j=0; j< masked.length; j++){
+                const ch = masked[j];
+                if(ch === ESCAPE_CHARACTER){
+                    j++;
+                    if(j< masked.length){
+                        unmasked += masked[j];
+                    }else{
+                        unmasked += ESCAPE_CHARACTER;
+                    }
+                }else{
+                    unmasked +=ch;
+                }
+            }
+            rawComponents.push(unmasked);
+        }
+        return rawComponents.join(delimiter);
     }
 
     public toString(): string {
@@ -22,25 +42,70 @@ export abstract class AbstractName implements Name {
     }
 
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        const rawComponents: string[] = [];
+        
+        //unescape each masked component to a raw string
+        for(let i=0; i<this.getNoComponents(); i++){
+            const masked = this.getComponent(i);
+            let unmasked = '';
+
+            for(let j=0; j< masked.length; j++){
+                const ch = masked[j];
+                if(ch === ESCAPE_CHARACTER){
+                    j++;
+                    if(j< masked.length){
+                        unmasked += masked[j];
+                    }else{
+                        unmasked += ESCAPE_CHARACTER;
+                    }
+                }else{
+                    unmasked +=ch;
+                }
+            }
+            rawComponents.push(unmasked);
     }
 
+    //re-mask for DEFAULT_DELIMITER and ESCAPE_CHARACTER
+    const maskedForDefault = rawComponents.map(raw =>{
+        let masked = '';
+        for(let j=0; j< raw.length; j++){
+            const ch= raw[j];
+            if(ch === ESCAPE_CHARACTER || ch === DEFAULT_DELIMITER){
+                masked += ESCAPE_CHARACTER;
+            }
+            masked+=ch;
+        }
+        return masked;
+    });
+    return maskedForDefault.join(DEFAULT_DELIMITER);
+}
+
+    /** Equality based on data representation */
     public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.asDataString() === other.asDataString();
     }
 
+    /** Forces 32-bit hash over the data string */
     public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
+        const data = this.asDataString();
+        let hash= 0;
+        for(let i=0; i<data.length; i++){
+            const ch= data.charCodeAt(i);
+            hash = ((hash << 5) - hash) +ch;
+            hash |=0;
+        }
+        return hash;
     }
 
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.getNoComponents() === 0;
     }
 
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        return this.delimiter;
     }
 
+    //concrete operations of subclasses
     abstract getNoComponents(): number;
 
     abstract getComponent(i: number): string;
@@ -51,7 +116,9 @@ export abstract class AbstractName implements Name {
     abstract remove(i: number): void;
 
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        for(let i=0; i< other.getNoComponents(); i++){
+            this.append(other.getComponent(i));
+        }
     }
 
 }
--- src/adap-b03/names/StringName.ts
+++ src/adap-b03/names/StringName.ts
@@ -8,64 +8,127 @@ export class StringName extends AbstractName {
     protected noComponents: number = 0;
 
     constructor(source: string, delimiter?: string) {
-        super();
-        throw new Error("needs implementation or deletion");
+        super(delimiter ?? DEFAULT_DELIMITER);
+        this.name = source;
+
+        //count components 
+        if (source === ""){
+        this.noComponents = 1;            // One empty component
+        }else{
+        this.noComponents = this.name.split(this.delimiter).length;
     }
+}
 
     public clone(): Name {
-        throw new Error("needs implementation or deletion");
+        return new StringName(this.name, this.delimiter);
     }
 
-    public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
-    }
+    /** asString, asDataString, isEqual, getHashCode, isEmpty, getDelimiterCharacter
+        are inherited from AbstractName.
+    */
 
-    public asDataString(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
+    /** return the number of components */
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.noComponents;
     }
 
-    public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+    /** return component at the given index */
+    public getComponent(x: number): string {
+        const components= this.name === '' ? []: this.name.split(this.delimiter);
+        if(x<0 || x>= components.length){
+            throw new RangeError('Index out of range');
+        }
+        return components[x];
+    }
+    
+    /** replace the component at the given index */
+    public setComponent(n: number, c: string): void {
+        const components= this.name === '' ? []: this.name.split(this.delimiter);
+        if(n<0 || n>= components.length){
+            throw new RangeError('Index out of range');
+        }
+        components[n]= c;
+
+        this.name = components.join(this.delimiter);
+        this.noComponents = components.length;
+
     }
 
-    public setComponent(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    /** insert the component at index specified */
+    public insert(n: number, c: string): void {
+        const components= this.name === '' ? []: this.name.split(this.delimiter);
+        if(n<0 || n> components.length){
+            throw new RangeError('Index out of range');
+        }
+        components.splice(n, 0, c);
+
+        this.name = components.join(this.delimiter);
+        this.noComponents = components.length;
+
     }
 
-    public insert(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    /** append the component at the end of name */
+    // 3 cases
+    // noComponents == 0
+    // noComponents == 1 && name == ""
+    // base case
+    public append(c: string): void {
+        if(this.noComponents === 0){
+            // No components
+            this.name = c;
+            this.noComponents = 1;
+        }else if(this.noComponents === 1 && this.name === ""){
+            // One empty component
+            this.name = this.delimiter + c;
+            this.noComponents = 2;
+        }else{
+            // Normal case
+            this.name += this.delimiter + c;
+            this.noComponents++;
+        }
     }
 
-    public append(c: string) {
-        throw new Error("needs implementation or deletion");
+    /** remove the component at the given index */
+    public remove(n: number): void {
+        let components: string[];
+        if(this.noComponents === 0){
+            components = [];
+        }else if(this.noComponents === 1 && this.name === ''){
+            //One empty component
+            components = ['']; 
+        }else{
+            components = this.name.split(this.delimiter);
+        }
+
+        //Catch error if-so: new 
+        if (n < 0 || n >= components.length) {
+            throw new RangeError("Index out of range");
+        }
+
+        components.splice(n, 1);
+        
+        // Manual reconstruction of the components dependent on their meaning
+        if(components.length === 0){
+            this.name = '';
+            this.noComponents = 0;
+        }else if(components.length === 1 && components[0] === ''){
+            this.name = '';
+            this.noComponents = 1;
+        }else{
+            this.name = components.join(this.delimiter);
+            this.noComponents = components.length;
+        }
     }
 
-    public remove(i: number) {
-        throw new Error("needs implementation or deletion");
-    }
-
+    /** concatenate components from another instance(Name) onto this one*/
     public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+        const components= this.name === '' ? []: this.name.split(this.delimiter);
+        for(let i=0; i< other.getNoComponents(); i++){
+            components.push(other.getComponent(i));
+        }
+        this.name = components.join(this.delimiter);
+        this.noComponents = components.length;
+
     }
 
 }
